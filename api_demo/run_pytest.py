e6a5d7d77dd81385bd001758f1741e4cf59d9c50
Jerry <lizion138@163.com>
2022-12-13 18:28:03 +0800

    test1

--- api_demo/run_pytest.py
+++ api_demo/run_pytest.py
@@ -9,7 +9,8 @@ def run_test():
     test_folder = os.path.join(currentPath , 'test')
     # print(test_folder)
     pytest.main([test_folder,'--alluredir=%s' %(report_path),'-o log_cli=true -o log_cli_level=INFO'])
+    os.system('allure generate %s -o %s -c' %(report_path,allure_report_path))
 
-    # os.system('C:\\Python34\\allure-2.7.0\\bin\\allure serve %s' %report_path)
-    os.system('C:\\Python34\\allure-2.7.0\\bin\\allure generate %s -o %s/html --clean' %(report_path,allure_report_path))    # 替换为本地的 allure 安装路径
 
+if __name__=='__main__':
+    run_test()
